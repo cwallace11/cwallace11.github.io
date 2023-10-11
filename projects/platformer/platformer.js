71f1f0130ddbbb953b7f3acdb1eb9b347fd32264
Cole Wallace <coleman0290@icloud.com>
2023-10-11 17:54:29 +0000

    <platformer>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -76,9 +76,10 @@ createCollectable('database', 600, 300, 2, .5)
 
 createCannon('top', 500, 1500, 10, 10)
 createCannon('right', 400, 2000, 10, 10)
-createCannon('top', 900, 2500, 10, 10)
+createCannon('top', 900, 2000, 10, 10)
 createCannon('left', 75, 2500, 10, 10)
 createCannon('left', 400, 1500, 10, 10)
+teleportTo(700,700)
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
